7c7c95c03622a58e22929212af45d178b83b4835
Vincent Raimondi <vraim001@ucr.edu>
2023-02-24 18:24:43 -0800

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Vincent Raimondi on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
